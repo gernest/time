c2191d16f5a5207df9d566b174aa05ac6a88ef9d
gernest <geofreyernest@live.com>
2018-11-12 20:16:00 +0300

    add now fn

--- src/time.zig
+++ src/time.zig
@@ -1,3 +1,38 @@
+const std = @import("std");
+const builtin = @import("builtin");
+const Os = builtin.Os;
+const debug = std.debug;
+
+const windows = std.os.windows;
+const linux = std.os.linux;
+const darwin = std.os.darwin;
+const posix = std.os.posix;
+
+// The unsigned zero year for internal calculations.
+// Must be 1 mod 400, and times before it will not compute correctly,
+// but otherwise can be changed at will.
+const absoluteZeroYear: i64 = -292277022399;
+
+// The year of the zero Time.
+// Assumed by the unixToInternal computation below.
+const internalYear: i64 = 1;
+
+// Offsets to convert between internal and absolute or Unix times.
+const absoluteToInternal: i64 = (absoluteZeroYear - internalYear) * 365.2425 * secondsPerDay;
+const internalToAbsolute = -absoluteToInternal;
+
+const unixToInternal: i64 = (1969 * 365 + 1969 / 4 - 1969 / 100 + 1969 / 400) * secondsPerDay;
+const internalToUnix: i64 = -unixToInternal;
+
+const wallToInternal: i64 = (1884 * 365 + 1884 / 4 - 1884 / 100 + 1884 / 400) * secondsPerDay;
+const internalToWall: i64 = -wallToInternal;
+
+const hasMonotonic = 1 << 63;
+const maxWall = wallToInternal + (1 << 33 - 1); // year 2157
+const minWall = wallToInternal; // year 1885
+const nsecMask = 1 << 30 - 1;
+const nsecShift = 30;
+
 pub const Time = struct.{
     wall: u54,
     ext: i64,
@@ -5,3 +40,32 @@ pub const Time = struct.{
 };
 
 pub const Loacation = struct.{};
+
+pub fn now() Time {
+    const bt = timeNow();
+    const sec = bt.sec + unixToInternal - minWall;
+    return Time.{ .wall = @intCast(u64, bt.nsec), .ext = @intCast(u64, sec + minWall) };
+}
+
+const bintime = struct.{
+    sec: isize,
+    nsec: isize,
+};
+
+fn timeNow() bintime {
+    switch (builtin.os) {
+        Os.linux => {
+            var ts: posix.timespec = undefined;
+            const err = posix.clock_gettime(posix.CLOCK_REALTIME, &ts);
+            debug.assert(err == 0);
+            return bintime.{ .sec = ts.tv_sec, .nsec = ts.tv_nsec };
+        },
+        Os.macosx, Os.ios => {
+            var tv: darwin.timeval = undefined;
+            var err = darwin.gettimeofday(&tv, null);
+            debug.assert(err == 0);
+            return bintime.{ .sec = ts.tv_sec, .nsec = ts.tv_usec };
+        },
+        else => @compileError("Unsupported OS"),
+    }
+}
