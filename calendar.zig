8b9951035e0ac0557add014c131cb8956144bdf5
gernest <geofreyernest@live.com>
2018-12-13 08:09:29 +0300

    remove unused imports

--- calendar.zig
+++ calendar.zig
@@ -1,8 +1,6 @@
 // Calendar is a simple zig program that prints the calendar on the stdout. This
 // is to showcase maturity of the time library.
 
-const std = @import("std");
-const warn = std.debug.warn;
 const Time = @import("./src/time.zig").Time;
 
 pub fn main() void {
